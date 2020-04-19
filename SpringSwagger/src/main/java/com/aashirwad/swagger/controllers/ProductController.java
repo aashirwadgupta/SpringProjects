dd0d4131474451853daf1458f1608f6e5f07f1b9
Aashirwad Gupta <aash.gupta2@gmail.com>
2020-04-19 20:02:52 +0530

    First Commit

--- SpringSwagger/src/main/java/com/aashirwad/swagger/controllers/ProductController.java
+++ SpringSwagger/src/main/java/com/aashirwad/swagger/controllers/ProductController.java
@@ -1,1 +1,32 @@
-package com.aashirwad.swagger;
+package com.aashirwad.swagger.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aashirwad.swagger.model.ProductModel;
+import com.aashirwad.swagger.service.ProductService;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+	
+	@Autowired
+	private ProductService productService;
+
+	@GetMapping("/getProducts")
+	public ResponseEntity<List<ProductModel>> getAllProducts() {
+		return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/getProduct/{productId}")
+	public ResponseEntity<ProductModel> getAProduct(@PathVariable("productId") Integer productId) {
+		return new ResponseEntity<>(productService.getAProduct(productId), HttpStatus.OK);
+	}
+}
